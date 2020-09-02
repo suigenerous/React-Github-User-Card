c8f3ec5db7272716988463f40bb4fd7f38d94c12
William Herman <williamajherman@gmail.com>
2020-09-01 21:46:04 -0400

    added helper functions

--- github_user_cards/src/App.js
+++ github_user_cards/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import constants from './constants'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  // constructor function
+  constructor(){
+    super();
+    this.state ={
+      dummystate: '',
+      userData: [],
+      followersData: []
+    }
+  }
+
+  // axios helper function - receives url, sets response data to state
+
+  axiosHelper(url){
+
+  }
+
+  // componentdidmount function calls axios helper after component has mounted
+
+  componentDidMount(){
+    return;
+  }
+
+  // render function
+  render(){
+    return(
+      <div>
+        <h1>Github Info App</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ github_user_cards/src/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+    userUrl: 'https://api.github.com/users/suigenerous',
+    followersUrl: 'https://api.github.com/users/suigenerous/followers'
+}
+
+export default constants
